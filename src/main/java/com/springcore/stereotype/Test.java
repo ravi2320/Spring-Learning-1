6e9f28c4e1ac362fe480a4b0a743f39001d7534f
Ravi2320 <rbkadam2320@gmail.com>
2024-06-09 14:51:51 +0530

    Spring Bean scope using xml and annotation

--- src/main/java/com/springcore/stereotype/Test.java
+++ src/main/java/com/springcore/stereotype/Test.java
@@ -9,11 +9,27 @@ public class Test {
 		// TODO Auto-generated method stub
 		ApplicationContext context = new ClassPathXmlApplicationContext("com/springcore/stereotype/stereo_config.xml");
 		Student student = context.getBean("student", Student.class);
-		System.out.println(student);
+		Student student2 = context.getBean("student", Student.class);
 		
-		System.out.println("_________________________________________________");
-		System.out.println(student.getState());
-		System.out.println(student.getState().getClass().getName());
+		/*
+		 * System.out.println(student);
+		 * 
+		 * System.out.println("_________________________________________________");
+		 * System.out.println(student.getState());
+		 * System.out.println(student.getState().getClass().getName());
+		 */
+		System.out.println("Prototype scope using annotation");
+		System.out.println("Hashcode of student object : " + student.hashCode());
+		System.out.println("Hashcode of student2 object : " + student2.hashCode());
+		System.out.println("__________________________________________________________");
+
+		System.out.println("Prototype scope using xml");
+		Teacher t1 = context.getBean("t1", Teacher.class);
+		Teacher t2 = context.getBean("t1", Teacher.class);
+		System.out.println("Hashcode of t1 object : " + t1.hashCode());
+		System.out.println("Hashcode of t2 object : " + t2.hashCode());
+		
+		
 	}
 
 }
